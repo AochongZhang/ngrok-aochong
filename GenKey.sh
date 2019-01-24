c760475e7a51550e07a84a27b1b21c6234f2497b
AochongZhang <aochongzhang@gmail.com>
2019-01-24 17:39:15 +0800

    update

---
+++ GenKey.sh
@@ -0,0 +1,14 @@
+NGROK_DOMAIN=$1
+# 生成证书
+echo 开始生成证书 域名=$1
+mkdir keys
+openssl genrsa -out keys/base.key 2048
+openssl req -new -x509 -nodes -key keys/base.key -days 10000 -subj "/CN=ngrok.k8s.kim" -out keys/base.pem
+openssl genrsa -out keys/server.key 2048
+openssl req -new -key keys/server.key -subj "/CN=ngrok.k8s.kim" -out keys/server.csr
+openssl x509 -req -in keys/server.csr -CA keys/base.pem -CAkey keys/base.key -CAcreateserial -days 10000 -out keys/server.crt
+
+# 替换证书
+cp keys/base.pem assets/client/tls/ngrokroot.crt
+
+echo 证书生成完毕
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ngrok-aochong
+
+### 在ngrok源码基础上增加自动配置构建脚本
+
+## 安装前环境
+
+#### 一台有公网ip的服务器
+
+#### go语言环境
+
+##### CentOS
+
+```shell
+yum install -y golang
+```
+
+##### macOS
+
+```shell
+brew install golang
+```
+
+#### git
+
+#### 科学上网
+
+## 安装步骤
+
+### 1. 克隆代码到本地
+
+```shell
+git clone https://github.com/AochongZhang/ngrok-aochong.git
+```
+
+### 2. 
